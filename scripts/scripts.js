ca3362644c9959effe3631785d2289be9df550c9
MikWah <mikwah@eeazy.se>
2024-12-13 22:00:28 +0100

    🐛 Fixed a bug using modules and require.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,42 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process');
-const path = require('path');
-const fs = require('fs');
+import { exec } from 'child_process';
+import { join } from 'path';
+import { mkdir, rm } from 'fs/promises';
+import { cwd, chdir } from 'process';
 
-const git_repo = 'https://github.com/CoffeeBreakDev/setup.git'
+const gitRepo = 'https://github.com/CoffeeBreakDev/setup.git';
 
 if (process.argv.length < 3) {
   console.log('You have to provide a name to your app.');
-  console.log('For example :');
+  console.log('For example:');
   console.log('    npx coffeebreakdev my-app');
   process.exit(1);
 }
 
 const projectName = process.argv[2];
-const currentPath = process.cwd();
-const projectPath = path.join(currentPath, projectName);
+const currentPath = cwd();
+const projectPath = join(currentPath, projectName);
+
+async function runCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error.message);
+      } else {
+        resolve(stdout.trim());
+      }
+    });
+  });
+}
 
 async function main() {
   try {
-    fs.mkdirSync(projectPath);
+    await mkdir(projectPath);
   } catch (err) {
     if (err.code === 'EEXIST') {
       console.log(
-        `The file ${projectName} already exist in the current directory, please give it another name.`
+        `The file ${projectName} already exists in the current directory. Please choose another name.`
       );
     } else {
       console.log(err);
@@ -33,18 +46,18 @@ async function main() {
 
   try {
     console.log('Downloading files...');
-      exec(`git clone --depth 1 ${git_repo} ${projectPath}`);
+    await runCommand(`git clone --depth 1 ${gitRepo} ${projectPath}`);
 
-    process.chdir(projectPath);
+    chdir(projectPath);
 
     console.log('Installing dependencies...');
-     exec('npm install');
+    await runCommand('npm install');
 
-    console.log('Removing useless files');
-    fs.rmSync('./.git', { recursive: true, force: true });
-    fs.rmSync(path.join(projectPath, 'scripts'), { recursive: true });
+    console.log('Removing unnecessary files...');
+    await rm(join(projectPath, '.git'), { recursive: true, force: true });
+    await rm(join(projectPath, 'scripts'), { recursive: true, force: true });
 
-    console.log('The installation is done, this is ready to use!');
+    console.log('The installation is done, and your project is ready to use!');
   } catch (error) {
     console.error('Error:', error);
   }
