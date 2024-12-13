743dbc4d09ad90c28014a632fae5552377d16f85
MikWah <mikwah@eeazy.se>
2024-12-13 22:04:38 +0100

    🚑 Fixed a bug with modules

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,9 @@ import { exec } from 'child_process';
 import { join } from 'path';
 import { mkdir, rm } from 'fs/promises';
 import { cwd, chdir } from 'process';
+import { promisify } from 'util';
 
+const execAsync = promisify(exec);
 const gitRepo = 'https://github.com/CoffeeBreakDev/setup.git';
 
 if (process.argv.length < 3) {
@@ -18,48 +20,38 @@ const projectName = process.argv[2];
 const currentPath = cwd();
 const projectPath = join(currentPath, projectName);
 
-async function runCommand(command) {
-  return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(error.message);
-      } else {
-        resolve(stdout.trim());
-      }
-    });
-  });
-}
-
 async function main() {
   try {
-    await mkdir(projectPath);
+    // Create project directory
+    await mkdir(projectPath, { recursive: false });
   } catch (err) {
     if (err.code === 'EEXIST') {
       console.log(
-        `The file ${projectName} already exists in the current directory. Please choose another name.`
+        `The project "${projectName}" already exists in the current directory. Please choose another name.`
       );
     } else {
-      console.log(err);
+      console.error(err);
     }
     process.exit(1);
   }
 
   try {
     console.log('Downloading files...');
-    await runCommand(`git clone --depth 1 ${gitRepo} ${projectPath}`);
+    await execAsync(`git clone --depth 1 ${gitRepo} ${projectPath}`);
 
     chdir(projectPath);
 
     console.log('Installing dependencies...');
-    await runCommand('npm install');
+    await execAsync('npm install');
 
     console.log('Removing unnecessary files...');
     await rm(join(projectPath, '.git'), { recursive: true, force: true });
     await rm(join(projectPath, 'scripts'), { recursive: true, force: true });
 
-    console.log('The installation is done, and your project is ready to use!');
+    console.log('The installation is complete. Your project is ready to use!');
   } catch (error) {
-    console.error('Error:', error);
+    console.error('An error occurred:', error);
+    process.exit(1);
   }
 }
 
